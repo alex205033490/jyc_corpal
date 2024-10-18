660ba5ba07bf0f46c032aa7b411f63dfa64a3923
Carlos Tapia <140286123+carlos-tapia96@users.noreply.github.com>
2024-10-18 16:45:01 -0400

    Create JSapi

---
+++ jycboliviaASP.net/js/jsApi.js
@@ -0,0 +1,23 @@
+﻿//-------------------------------   FUNCION AGREGAR FILAS FORM
+function addRowIngreso() {
+    var table = document.getElementById("tblDetalleProductosIngresos");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    row.innerHTML =
+        `
+    <td><input type='text' class="form-control" name='codigoProducto${rowCount}' /></td>
+    <td><input type='number' class="form-control" name='unidadMedida${rowCount}' /></td>
+    <td><input type='number' class="form-control" step="0.01" name='cantidad${rowCount}' /></td>
+    <td><input type='number' class="form-control" step="0.01" name='costoUnitario${rowCount}' /></td>`;
+}
+
+
+function addRowEgreso() {
+    var table = document.getElementById("tblDetalleProductosEgresos");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    row.innerHTML =
+        `<td><input type='text' class="form-control" name='codigoProducto${rowCount}' /></td>
+         <td><input type='number' class="form-control" name='unidadMedida${rowCount}' /></td>
+         <td><input type='number' class="form-control" step="0.01" name='cantidad${rowCount}' /></td>`;
+}
